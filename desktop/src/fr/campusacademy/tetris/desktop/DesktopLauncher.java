e26e8073843b8b57fdc9acd200abc5c8bec08561
MathisD49 <mathisdubuc@gmail.com>
2021-03-08 10:32:56 +0100

    ajout LibGDX

---
+++ desktop/src/fr/campusacademy/tetris/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.campusacademy.tetris.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.campusacademy.tetris.Tetris;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Tetris(), config);
+	}
+}
