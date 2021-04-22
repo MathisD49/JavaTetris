4abda7b2d091a9e9e9e0b3b5095c21951d0e960e
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2021-04-22 18:05:24 +0200

    menu

---
+++ src/fr/campusacademy/tetris/game/Menu.java
@@ -0,0 +1,77 @@
+package fr.campusacademy.tetris.game;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+
+public class Menu extends JFrame implements ActionListener {
+
+	JButton button;
+
+	public Menu() {
+
+		ImageIcon playIcon = new ImageIcon("assets/playbutton.png");
+		ImageIcon quitIcon = new ImageIcon("assets/quitbutton.png");
+
+		JButton playButton = new JButton();
+		playButton.setBounds(100, 200, 200, 100);
+		playButton.addActionListener(e -> playGame());
+		playButton.addActionListener(e -> removeMenu());
+		playButton.setFocusable(false);
+		playButton.setIcon(playIcon);
+
+		JButton quitButton = new JButton();
+		quitButton.setBounds(400, 200, 200, 100);
+		quitButton.addActionListener(e -> System.exit(0));
+		quitButton.setFocusable(false);
+		quitButton.setIcon(quitIcon);
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(null);
+		this.setSize(700, 500);
+		this.setVisible(true);
+		this.add(playButton);
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(null);
+		this.setSize(700, 500);
+		this.setVisible(true);
+		this.add(quitButton);
+
+	}
+
+	void playGame() {
+		try {
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new SimpleSlickGame("Tetris"));
+			appgc.setDisplayMode(700, 900, false);
+			appgc.setTargetFrameRate(4);
+			Input.disableControllers();
+			appgc.start();
+		} catch (SlickException ex) {
+			Logger.getLogger(SimpleSlickGame.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
+	void removeMenu() {
+		this.setVisible(false);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+}
--- src/fr/campusacademy/tetris/launcher/Launcher.java
+++ src/fr/campusacademy/tetris/launcher/Launcher.java
@@ -7,24 +7,15 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
+import fr.campusacademy.tetris.game.Menu;
 import fr.campusacademy.tetris.game.SimpleSlickGame;
 import fr.campusacademy.tetris.game.TestWindow;
 
 public class Launcher {
 	public static void main(String[] args)
-	{
-		try
-		{
-			AppGameContainer appgc;
-			appgc = new AppGameContainer(new SimpleSlickGame("Tetris"));
-			appgc.setDisplayMode(700, 900, false);
-			appgc.setTargetFrameRate(4);
-			Input.disableControllers();
-			appgc.start();
-		}
-		catch (SlickException ex)
-		{
-			Logger.getLogger(SimpleSlickGame.class.getName()).log(Level.SEVERE, null, ex);
-		}
+	{	
+		
+	Menu menu = new Menu();
+	
 	}
 }
